d0da77c25bf9bada19b84361f9190bb60c267ed1
Unknown <james850704@gmail.com>
2019-04-25 15:02:38 +0800

    add palindrome number

---
+++ 9. Palindrome Number/src/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int origin=x;
+        int reverse=0;
+          if (x<0 || (x!=0 && x%10==0)) return false;
+        while(x>0){
+                reverse=reverse*10+x%10;
+                x=x/10;
+        }
+      return (origin==reverse);
+    }
+}
